a6f852921cb83f04b5d4995a4431ef685e56d1a8
notkhalid <khalidallib008@gmail.com>
2023-01-18 11:05:41 +0100

    Update counterjs.js

--- word counter/counterjs.js
+++ word counter/counterjs.js
@@ -1,6 +1,6 @@
 var button = document.getElementById("submit");
 var answer = document.getElementById("answer");
-clear = document.getElementById("clear")
+var clear = document.getElementById("clear")
 
 button.addEventListener("click", function() {
     var input = document.getElementById("inptf").value.trim();
